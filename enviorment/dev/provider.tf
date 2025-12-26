5f8c9d821378f070c7d107cc947f4bac51112b3a
Vsondhiya <vsondhiya@gmail.com>
2025-12-27 00:44:28 +0530

    Update Azure backend configuration and subscription ID

--- enviorment/dev/provider.tf
+++ enviorment/dev/provider.tf
@@ -5,15 +5,15 @@ terraform {
       version = "4.49.0"
     }
   }
-  # backend "azurerm" {
-  #   resource_group_name  = "rg-dhondu"
-  #   storage_account_name = "tfstatesdhondhuwala"
-  #   container_name       = "tfstates"
-  #   key                  = "dev.terraform.tfstate"
-  # }
+  backend "azurerm" {
+   resource_group_name  = "dev-rg"
+   storage_account_name = "vinodstg123"
+   container_name       = "vinodcontainer"
+    key                  = "dev.terraform.tfstate"
+   }
 }
 
 provider "azurerm" {
   features {}
-  subscription_id = "1b05e470-de80-481a-8feb-07829124ebd9"
+  subscription_id = "f4364164-2e58-4621-8dd2-b48e519fd047"
 }
